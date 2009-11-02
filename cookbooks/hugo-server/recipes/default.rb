8835013bbd084c1b7fd2e3e42f76557348928e7a
P. Barrett Little <barrett@designercode.com>
2009-11-01 22:30:37 -0500

    Fixed hugo server recipe bug.

--- cookbooks/hugo-server/recipes/default.rb
+++ cookbooks/hugo-server/recipes/default.rb
@@ -2,16 +2,13 @@ include_recipe "apache2"
 include_recipe "passenger_apache2::mod_rails"
 include_recipe "sinatra"
 
-
 require 'chef-deploy'
 
-
-chef-deploy "/apps/hugo-server" do
+deploy "/apps/hugo-server" do
   repo "git://github.com/twilson63/hugo-server.git"
   branch "HEAD"
   environment "production"
   restart_command "touch tmp/restart.txt"
   shallow_clone true
   action :deploy
-
 end
