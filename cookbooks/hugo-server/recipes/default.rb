7a99fccb995d760daa2d951ba0107c80404eee73
P. Barrett Little <barrett@designercode.com>
2009-11-02 20:57:03 -0500

    Fixed chef-deploy bug caused by app directory not existing before deploy.

--- cookbooks/hugo-server/recipes/default.rb
+++ cookbooks/hugo-server/recipes/default.rb
@@ -2,9 +2,16 @@ include_recipe "apache2"
 include_recipe "passenger_apache2::mod_rails"
 include_recipe "sinatra"
 
-require 'chef-deploy'
+# require 'chef-deploy'
 
-deploy "/apps/hugo-server" do
+appname = "hugo-server"
+
+directory "/home/ubuntu/apps/#{appname}" do
+  action :create
+  recursive true
+end
+
+deploy "/home/ubuntu/apps/#{appname}" do
   repo "git://github.com/twilson63/hugo-server.git"
   branch "HEAD"
   environment "production"
