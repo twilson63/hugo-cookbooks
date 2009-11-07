ce5111b1c818bd49ca13f134945cf61c813fd881
Tom Wilson <tom@jackhq.com>
2009-11-07 11:53:10 -0500

    create ssh dir

--- cookbooks/github_keys/recipes/default.rb
+++ cookbooks/github_keys/recipes/default.rb
@@ -1,17 +1,17 @@
-directory '~/.ssh' do
+directory '/home/ubuntu/.ssh' do
   owner "ubuntu"
   group "ubuntu"
   mode "0755"
   action :create
 end
 
-template "~/.ssh/id_rsa" do
+template "/home/ubuntu/.ssh/id_rsa" do
   mode 0644
   source "privatekey.erb"
   variables :privatekey => node[:github][:privatekey]
 end
 
-template "~/.ssh/id_rsa.pub" do
+template "/home/ubuntu/.ssh/id_rsa.pub" do
   mode 0644
   source "publickey.erb"
   variables :publickey => node[:github][:publickey]
