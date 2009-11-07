17e03fdf3fd9a182739a078ffd7d4aadf0e9237f
Tom Wilson <tom@jackhq.com>
2009-11-07 11:49:06 -0500

    fixed github key templates

--- cookbooks/github_keys/recipes/default.rb
+++ cookbooks/github_keys/recipes/default.rb
@@ -1,9 +1,11 @@
 template "~/.ssh/id_rsa" do
   mode 0644
   source "privatekey.erb"
+  variables :privatekey => node[:github][:privatekey]
 end
 
 template "~/.ssh/id_rsa.pub" do
   mode 0644
   source "publickey.erb"
+  variables :publickey => node[:github][:publickey]
 end
