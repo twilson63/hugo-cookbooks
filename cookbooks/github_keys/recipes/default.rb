03d4773e95eed86c62d7252d0442d61ac6682741
P. Barrett Little <barrett@designercode.com>
2009-11-17 13:52:21 -0500

    Tweaked key permissions and add ssh config template.

--- cookbooks/github_keys/recipes/default.rb
+++ cookbooks/github_keys/recipes/default.rb
@@ -1,23 +1,30 @@
 directory '/home/ubuntu/.ssh' do
   owner "ubuntu"
   group "ubuntu"
-  mode "0755"
+  mode "0700"
   action :create
   recursive true
 end
 
-template "/home/ubuntu/.ssh/hugo_rsa" do
+template "/home/ubuntu/.ssh/id_rsa" do
   owner "ubuntu"
   group "ubuntu"
-  mode 0600
+  mode "0600"
   source "privatekey.erb"
   variables :privatekey => node[:github][:privatekey]
 end
 
-template "/home/ubuntu/.ssh/hugo_rsa.pub" do
+template "/home/ubuntu/.ssh/id_rsa.pub" do
   owner "ubuntu"
   group "ubuntu"
-  mode 0600
+  mode "0600"
   source "publickey.erb"
   variables :publickey => node[:github][:publickey]
+end
+
+template "/home/ubuntu/.ssh/config" do
+  owner "ubuntu"
+  group "ubuntu"
+  mode "0600"
+  source "config.erb"
 end
