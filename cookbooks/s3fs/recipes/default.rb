fed9bce640d3c0d7677660ea51f66dc9dc32c775
Tom Wilson <tom@jackhq.com>
2009-11-24 19:59:10 -0500

    added s3fs recipe

---
+++ cookbooks/s3fs/recipes/default.rb
@@ -0,0 +1,15 @@
+remote_file "/tmp/s3fs-r177-source.tar.gz" do
+  source "http://s3fs.googlecode.com/files/s3fs-r177-source.tar.gz"
+  mode 0644
+end
+
+bash "install s3fs" do
+  cwd "/tmp"
+  code <<-EOH
+  tar zxvf s3fs-r177-source.tar.gz
+  cd s3fs-r177-source
+  make
+  make install
+  EOH
+  not_if { File.exists?("/tmp/s3fs-r177-source.tar.gz") }
+end
