0fb9e5fe0237f28cc3ede7b57a555b9eff67ffde
Tom Wilson <tom@jackhq.com>
2009-11-25 15:28:12 -0500

    added jasper server recipe and role

---
+++ cookbooks/jasper_server/recipes/default.rb
@@ -0,0 +1,20 @@
+%w{ unzip openjdk-6-jdk }.each do |pkg|
+  package pkg
+end
+# http://sourceforge.net/projects/jasperserver/files/JasperServer/JasperServer%203.7.0%20RC/jasperserver-ce-3.7.0-RC-bin.zip/download
+
+remote_file "/tmp/jasperserver-ce-3.7.0-RC-bin.zip" do
+  source "http://sourceforge.net/projects/jasperserver/files/JasperServer/JasperServer%203.7.0%20RC/jasperserver-ce-3.7.0-RC-linux-installer.bin/download"
+  mode 0644
+end
+
+bash "install jasper server" do
+  cwd "/tmp"
+  code <<-EOH
+  chmod +x jasperserver-ce-3.7.0-RC-bin
+  echo -en \n\n\n\ny\n\n\n\n\n\n\nn\nn\n\nn\n | ./jasperserver-ce-3.7.0-RC-bin
+  cd jasperserver-ce-3.7.0
+  ./jasperctrl.sh start
+  EOH
+  not_if { File.exists?("/tmp/jasperserver-ce-3.7.0") }
+end
