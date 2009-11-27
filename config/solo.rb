52dbcd0785b5b25297ee281d03a1e09874104a92
Tom Wilson <tom@jackhq.com>
2009-11-27 13:35:40 -0500

    added site-cookbooks to solo

--- config/solo.rb
+++ config/solo.rb
@@ -1,4 +1,4 @@
 file_cache_path File.join(File.dirname(__FILE__), '..')
 cookbook_path File.join(File.dirname(__FILE__), '..', "cookbooks")
 role_path File.join(File.dirname(__FILE__), '..', "roles")
-
+site_cookbook_path File.join(File.dirname(__FILE__), '..', "site-cookbooks")
