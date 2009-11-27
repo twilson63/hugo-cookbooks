b8fa304b473f4fbfb2bb7a8d4d62cc5173cfcc38
Tom Wilson <tom@jackhq.com>
2009-11-27 14:31:26 -0500

    removed jasper bash script

--- cookbooks/jasper_server/recipes/default.rb
+++ cookbooks/jasper_server/recipes/default.rb
@@ -8,13 +8,14 @@ remote_file "/tmp/jasperserver-ce-3.7.0-RC-bin" do
   mode 0644
 end
 
-bash "install jasper server" do
-  cwd "/tmp"
-  code <<-EOH
-  chmod +x jasperserver-ce-3.7.0-RC-bin
-  echo -en "\n\n\n\ny\n\n\n\n\n\n\nn\nn\n\nn\n" | ./jasperserver-ce-3.7.0-RC-bin
-  cd jasperserver-ce-3.7.0
-  ./jasperctrl.sh start
-  EOH
-  not_if { File.exists?("/tmp/jasperserver-ce-3.7.0") }
-end
+# Not working need to spend time to correct!
+# bash "install jasper server" do
+#   cwd "/tmp"
+#   code <<-EOH
+#   chmod +x jasperserver-ce-3.7.0-RC-bin
+#   echo -en "\n\n\n\ny\n\n\n\n\n\n\nn\nn\n\nn\n" | ./jasperserver-ce-3.7.0-RC-bin
+#   cd jasperserver-ce-3.7.0
+#   ./jasperctrl.sh start
+#   EOH
+#   not_if { File.exists?("/home/ubuntu/jasperserver-ce-3.7.0") }
+# end
