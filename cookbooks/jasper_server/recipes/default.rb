48f4cac0ffa61d71e98999ee8530ba3dbb4a9ecd
Tom Wilson <tom@jackhq.com>
2010-01-29 23:01:18 -0500

    update hugo with jasper cookbooks

--- cookbooks/jasper_server/recipes/default.rb
+++ cookbooks/jasper_server/recipes/default.rb
@@ -2,20 +2,19 @@
   package pkg
 end
 # http://sourceforge.net/projects/jasperserver/files/JasperServer/JasperServer%203.7.0%20RC/jasperserver-ce-3.7.0-RC-bin.zip/download
-
-remote_file "/tmp/jasperserver-ce-3.7.0-RC-bin" do
-  source "http://sourceforge.net/projects/jasperserver/files/JasperServer/JasperServer%203.7.0%20RC/jasperserver-ce-3.7.0-RC-linux-installer.bin/download"
+remote_file "/home/ubuntu/apps/jasperserver-3.5.0-linux-installer.bin" do
+  source "http://downloads.sourceforge.net/project/jasperserver/JasperServer/JasperServer%203.5.0/jasperserver-3.5.0-linux-installer.bin?use_mirror=hivelocity"
   mode 0644
 end
 
 # Not working need to spend time to correct!
 # bash "install jasper server" do
-#   cwd "/tmp"
+#   cwd "/home/ubuntu/apps"
 #   code <<-EOH
-#   chmod +x jasperserver-ce-3.7.0-RC-bin
-#   echo -en "\n\n\n\ny\n\n\n\n\n\n\nn\nn\n\nn\n" | ./jasperserver-ce-3.7.0-RC-bin
-#   cd jasperserver-ce-3.7.0
+#   chmod +x jasperserver-3.5.0-linux-installer.bin
+#   echo -en "\n\n\n\ny\n\n\n\n\n\n\nn\nn\n\nn\n" | ./jasperserver-3.5.0-linux-installer.bin
+#   cd jasperserver-3.5.0
 #   ./jasperctrl.sh start
 #   EOH
-#   not_if { File.exists?("/home/ubuntu/jasperserver-ce-3.7.0") }
+#   not_if { File.exists?("/home/ubuntu/apps/jasperserver-3.5.0") }
 # end
